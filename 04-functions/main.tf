ea11476525232322ff3da908e41814dd69f219a7
Khyathi304 <khyathikamireddy04@gmail.com>
2023-08-03 17:07:40 +0100

    Terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -6,3 +6,14 @@ output "fruits" {
 #  value = var.fruits[2]
   value = element(var.fruits, 2)
 }
+
+variable "fruits_stock" {
+  default = {
+    apple = 100
+  }
+}
+
+output "fruit" {
+  #  value = var.fruits[2]
+  value = var.fruits_stock["banana"]
+}
