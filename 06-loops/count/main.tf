7673cde1b6ac252c7b9bcc4001a824035d608538
Khyathi304 <khyathikamireddy04@gmail.com>
2023-08-04 14:35:06 +0100

    Terraform

---
+++ 06-loops/count/main.tf
@@ -0,0 +1,14 @@
+variable "components" {
+  default = [ "frontend", "mongodb" ]
+}
+
+resource "aws_instance" "instance" {
+  count = length(var.components)
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0e9bb0f3fd84f7811" ]
+
+  tags = {
+    Name = ""
+    }
+  }
