fef159cc94891ab99eb4d5779b238b4585303cd7
Khyathi304 <khyathikamireddy04@gmail.com>
2023-08-04 15:43:11 +0100

    Terraform

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = var.security_groups
 
   tags = {
-    Name = lookup(each.value["name"], "null" )
+    Name = lookup(each.value, "name", "null" )
   }
 }
 
