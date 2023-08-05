c36eb6ef95853f40d874d9ecfb35a3343cbe40a4
Khyathi304 <khyathikamireddy04@gmail.com>
2023-08-05 16:01:58 +0100

    Terraform

---
+++ roboshop-v3/vars.tf
@@ -0,0 +1,19 @@
+variable "security_groups" {
+  default = [ "sg-0e9bb0f3fd84f7811" ]
+}
+variable "zone_id" {
+  default = "Z01685992MRA8PYOVXM0X"
+}
+
+variable "components" {
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.micro"
+    }
+    cart = {
+      name = "cart"
+      instance_type = "t3.micro"
+    }
+  }
+}
